5e030b125020b541e910e2f2ea433e6b26b0f375
Michael Littleton <michaelglittleton@gmail.com>
2021-03-14 14:10:27 -0400

    Setup user db migration

--- data/migrations/20210314134009_users.js
+++ data/migrations/20210314134009_users.js
@@ -1,10 +1,14 @@
-
-exports.up = function(knex) {
-
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('username').notNullable().unique();
+    table.string('password').notNullable();
+    table.boolean('sharing')
+  });
 };
 
-exports.down = function(knex) {
-
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users');
 };
 
 /*
